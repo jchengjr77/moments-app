461b0fa66bd9687d6e316464a119744b862730ac
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-02-03 17:19:21 -0500

    Cleaning Branch

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
-    width: Dimensions.get("window").width
+    width: Dimensions.get("window").width 
   }
 });
 
