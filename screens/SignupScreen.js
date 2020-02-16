90c119ed05b7e3d2e61d37f5671556b7fb187db6
Jonathan Cheng <jonathanchengjr77@gmail.com>
2020-02-16 15:28:51 -0500

    Finished Signup page to have three text inputs.
    Text inputs save state, can modify state on keystroke, and print state on submit.
    Input fields have default values, focus highlighting, scrollable view for keyboard avoidance, etc.
    Password field has secure text input enabled

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,91 +1,118 @@
 /**
- * ! @Vignesh comment the file haha
+ * Login screen should contain an username and password fields only.
+ * Sends a request to the database searching for the username and password.
  */
 
-import React from "react";
-import { View, StyleSheet, ImageBackground, Dimensions } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+  Dimensions,
+  ScrollView,
+  KeyboardAvoidingView
+} from "react-native";
 import BigPrimaryButton from "../components/BigPrimaryButton";
-import AwesomeButton from "react-native-really-awesome-button";
+
 import colors from "../constants/Colors";
 import pages from "../constants/Pages";
 import splashImg from "../assets/splash.png";
 
-const SignupScreen = props => {
-  let buttonWidth = Dimensions.get("window").width * 0.75;
-
-  const emailHandler = () => {
-    console.log("Email Button Pressed");
-  };
-
-  const facebookHandler = () => {
-    console.log("Facebook Button Pressed");
-  };
-
-  const googleHandler = () => {
-    console.log("Google Button Pressed");
-  };
-
-  const backHandler = () => {
-    console.log("Back Button Pressed");
-    props.switchHandler(pages.startPage);
-  };
+const LoginScreen = props => {
+  const [email, onChangeEmail] = useState("");
+  const [usrvalue, onChangeTextUsr] = useState("");
+  const [pwvalue, onChangeTextPw] = useState("");
+  const [selected1, setSelected1] = useState(false);
+  const [selected2, setSelected2] = useState(false);
+  const [selected3, setSelected3] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
 
   return (
-    <View style={styles.container}>
+    <ScrollView
+      contentContainerStyle={styles.screenContainer}
+      scrollEnabled={selected1 || selected2 || selected3}
+      contentOffset={offset}
+    >
       <ImageBackground style={styles.splash} source={splashImg}>
         <View style={styles.buttonCont}>
-          <BigPrimaryButton
-            title="Email"
-            onPress={emailHandler}
-            width={buttonWidth}
-            //height = {buttonHeight}
+          <TextInput
+            defaultValue={email}
+            placeholder="Email"
+            placeholderTextColor="#AAAAAA"
+            onChangeText={text => onChangeEmail(text)}
+            style={{
+              ...styles.inputField,
+              backgroundColor: selected1 ? "#F0F2F4" : "#DDDDDD"
+            }}
+            onFocus={() => {
+              setSelected1(true);
+              setOffset({ x: 0, y: 150 });
+            }}
+            onBlur={() => {
+              setSelected1(false);
+              setOffset({ x: 0, y: 0 });
+            }}
           />
-          <AwesomeButton
-            backgroundColor="#3b5998"
-            backgroundDarker="#193776"
-            backgroundShadow="#8b9dc3"
-            raiseLevel={4}
-            textFontFamily="montserrat-bold"
-            textColor="#ffffff"
-            textSize={20}
-            onPress={facebookHandler}
-            width={buttonWidth}
-            //height = {buttonHeight}
-          >
-            Facebook
-          </AwesomeButton>
-
-          <AwesomeButton
-            backgroundColor="#ffffff"
-            backgroundDarker="#64a7F6"
-            backgroundShadow="#eeeeee"
-            raiseLevel={4}
-            textFontFamily="montserrat-bold"
-            textColor="#333333"
-            textSize={20}
-            onPress={googleHandler}
-            width={buttonWidth}
-            //height = {buttonHeight}
-          >
-            Google
-          </AwesomeButton>
-        </View>
-        <View>
+          <TextInput
+            value={usrvalue}
+            placeholder="Username"
+            placeholderTextColor="#AAAAAA"
+            onChangeText={text => onChangeTextUsr(text)}
+            style={{
+              ...styles.inputField,
+              backgroundColor: selected2 ? "#F0F2F4" : "#DDDDDD"
+            }}
+            onFocus={() => {
+              setSelected2(true);
+              setOffset({ x: 0, y: 150 });
+            }}
+            onBlur={() => {
+              setSelected2(false);
+              setOffset({ x: 0, y: 0 });
+            }}
+          />
+          <TextInput
+            defaultValue={pwvalue}
+            placeholder="Password"
+            placeholderTextColor="#AAAAAA"
+            onChangeText={text => onChangeTextPw(text)}
+            style={{
+              ...styles.inputField,
+              backgroundColor: selected3 ? "#F0F2F4" : "#DDDDDD"
+            }}
+            onFocus={() => {
+              setSelected3(true);
+              setOffset({ x: 0, y: 150 });
+            }}
+            onBlur={() => {
+              setSelected3(false);
+              setOffset({ x: 0, y: 0 });
+            }}
+            secureTextEntry // because it is a password
+            selectTextOnFocus
+          />
           <BigPrimaryButton
-            title="↤"
-            onPress={backHandler}
-            width={buttonWidth / 6}
-            height={Dimensions.get("window").height * 0.05}
-            paddingTop={Dimensions.get("window").height / 7}
+            title="Submit"
+            onPress={() => {
+              console.log(
+                "Signup with username: " +
+                  usrvalue +
+                  ", password: " +
+                  pwvalue +
+                  ", email: " +
+                  email
+              );
+            }}
           />
         </View>
       </ImageBackground>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
@@ -102,7 +129,15 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     width: "100%",
     height: "75%"
+  },
+  inputField: {
+    borderColor: colors.primary,
+    borderBottomWidth: 2,
+    borderRadius: 5,
+    paddingLeft: 10,
+    width: Dimensions.get("window").width * 0.8,
+    height: "10%"
   }
 });
 
-export default SignupScreen;
+export default LoginScreen;
